054296cd6c4627de1db696f46037b981554274c0
guiijanuario <guilherme.januario@zup.com.br>
2023-06-13 00:00:44 -0300

    :boom: - Subindo projeto Dias dos Namorados

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n[------------------------------------]");
+        System.out.println("[ Feliz Dia dos Namorados, meu amor! ]");
+        System.out.println("[------------------------------------]\n");
+
+        String coracao =
+                " |###        ###|\n" +
+                        "|######    ######|\n" +
+                        "|#################|\n" +
+                        " |##############|\n" +
+                        "  |############|\n" +
+                        "    |#########|\n" +
+                        "      |#####|\n" +
+                        "        |###|\n" +
+                        "         |#|\n";
+
+        System.out.println(coracao);
+        System.out.println(coracao);
+        System.out.println(coracao);
+        System.out.println("[---------------]");
+        System.out.println("[ Te amo muito! ]");
+        System.out.println("[---------------]");
+        }
+    }
